6d375a7d104309c6fa9112cd020d2a1999fed8d2
himelmia535 <himelmia535@gmail.com>
2024-04-17 16:52:00 +0600

    handleSocialLogin location

--- src/components/PrivateDetails.jsx
+++ src/components/PrivateDetails.jsx
@@ -1,11 +1,12 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import Login from "../pages/Login";
 
 const PrivateDetails = ({children}) => {
     const {user} = useAuth();
+    const location = useLocation();
     if(!user){
-        return <Navigate to='/login'></Navigate>
+        return <Navigate to='/login' state={location ?.pathname || '/'}></Navigate>
     }
     return (
         <div>
