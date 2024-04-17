78ef7bee3a1fc778a74cf88cf861f79c895f0a99
himelmia535 <himelmia535@gmail.com>
2024-04-17 21:08:32 +0600

    animate and AOS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import FirebaseProvider from "../FirebaseProvider/FirebaseProvider";
 import Blogs from "../components/Blogs";
 import Slider from "../components/Slider";
+import 'animate.css';
 
 const Home = () => {
     return (
         <div className="mx-6">
             <Slider></Slider>
             <div className="text-center m-10">
-                <h2 className="text-base font-bold lg:text-5xl">Find Your Dream Home</h2>
+                <h2 className="animate__animated animate__zoomIn text-base font-bold lg:text-5xl">Find Your Dream Home</h2>
             </div>
             <Blogs></Blogs>
             <FirebaseProvider></FirebaseProvider>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,10 @@
+import 'animate.css';
 
 const Profile = () => {
     return (
         <div className="">
       <div>
-        <h2 className="text-2xl text-center font-bold m-5">Update your Profile Here:</h2>
+        <h2 className="text-2xl text-center font-bold m-5 animate__animated animate__bounce">Update your Profile Here:</h2>
       </div>
       <div className="justify-center flex ">
       <form className="m-4 w-72 p-6 border rounded-2xl border-opacity-30 border-primary hover:border-secondary">
