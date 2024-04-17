b1f64207a32143a09275dc6db7c750ab524bd146
himelmia535 <himelmia535@gmail.com>
2024-04-17 09:52:54 +0600

    Password verification

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
   </div>
   <div className="navbar-end gap-3">
    {
-    user?.email? <div className="dropdown dropdown-end">
+    user? <div className="dropdown dropdown-end">
       <label tabIndex={0} className="btn btn-ghost btn-circle avatar" >
         <div className="w-12 rounded-full">
             <img src={user.photoURL} alt="" />
@@ -41,14 +41,18 @@ const Navbar = () => {
           <button className="btn btn-sm btn-ghost">{user.displayName}</button>
         </li>
         <li>
-          <button onClick={logout} className="btn btn-sm btn-ghost">Logout</button>
+          <button onClick={logout} className="btn btn-sm btn-ghost hover:text-red-500">Logout</button>
         </li>
       </ul>
       </div>
       :
-        <Link to='/login'>
-          <button className="btn btn-sm btn-ghost">Login</button>
-        </Link>
+      <Link to='/login' className="relative inline-flex items-center px-10 py-3 overflow-hidden text-lg font-medium text-indigo-600 border-2 border-indigo-600 rounded-full hover:text-white group hover:bg-gray-50">
+      <span className="absolute left-0 block w-full h-0 transition-all bg-indigo-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+      <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+      <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+      </span>
+      <span className="relative">Login</span>
+      </Link>
    }
   </div>
 </div>
