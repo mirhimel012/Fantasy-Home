91a98c63e8fba0fb4600c92df5658e3b0432641d
himelmia535 <himelmia535@gmail.com>
2024-04-18 22:09:49 +0600

    Update profile info

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,20 +1,44 @@
 import 'animate.css';
+import { useState } from "react";
+import useAuth  from "../hooks/useAuth";
 
 const Profile = () => {
+  const { user, updateUserProfile } = useAuth();
+  const [name, setName] = useState(user.displayName || "");
+  const [photoURL, setPhotoURL] = useState(user.photoURL || "");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handlePhotoURLChange = (e) => {
+    setPhotoURL(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await updateUserProfile(name, photoURL);
+      alert("Profile updated successfully!");
+    } catch (error) {
+      console.error("Error updating profile:", error);
+      alert("Failed to update profile. Please try again later.");
+    }
+  };
     return (
         <div className="">
       <div>
         <h2 className="text-2xl text-center font-bold m-5 animate__animated animate__bounce">Update your Profile Here:</h2>
       </div>
       <div className="justify-center flex ">
-      <form className="m-4 w-72 p-6 border rounded-2xl border-opacity-30 border-primary hover:border-secondary">
+      <form onSubmit={handleSubmit} className="m-4 w-72 p-6 border rounded-2xl border-opacity-30 border-primary hover:border-secondary">
         <div className="mb-4">
           <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
           <input
             type="text"
             id="name"
             value={name}
-            // onChange={handleNameChange}
+            onChange={handleNameChange}
             className="mt-1 p-2 block w-full border rounded-md"
           />
         </div>
@@ -23,8 +47,8 @@ const Profile = () => {
           <input
             type="text"
             id="photoURL"
-            // value={photoURL}
-            // onChange={handlePhotoURLChange}
+            value={photoURL}
+            onChange={handlePhotoURLChange}
             className="mt-1 p-2 block w-full border rounded-md"
           />
         </div>
