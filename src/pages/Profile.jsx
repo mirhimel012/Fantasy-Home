27d8ac1cd5fcfde94404d679623d0d85487e6194
himelmia535 <himelmia535@gmail.com>
2024-04-18 22:36:04 +0600

    Your Information Displayed

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -26,12 +26,22 @@ const Profile = () => {
     }
   };
     return (
-        <div className="">
+        <div className="flex-col flex justify-center items-center lg:flex-row">
+          <div className="m-4 p-6 border rounded-2xl border-opacity-30 border-primary hover:border-secondary">
+    <h2 className="text-2xl text-center font-bold m-5 animate__animated animate__bounce">Your Information:</h2>
+    <p className="text-gray-700"><span className='font-semibold'>Name:</span> {user.displayName}</p>
+    <p className="text-gray-700"><span className='font-semibold'>Email:</span> {user.email}</p>
+    <div className='flex justify-center m-6'>
+      <img src={user.photoURL} alt="Profile" className="w-32 h-32 rounded-full mt-4" />
+    </div>
+        </div>
+
+        <div className="m-4 w-72 p-6 border rounded-2xl border-opacity-30 border-primary hover:border-secondary">
       <div>
         <h2 className="text-2xl text-center font-bold m-5 animate__animated animate__bounce">Update your Profile Here:</h2>
       </div>
-      <div className="justify-center flex ">
-      <form onSubmit={handleSubmit} className="m-4 w-72 p-6 border rounded-2xl border-opacity-30 border-primary hover:border-secondary">
+      <div className="">
+      <form onSubmit={handleSubmit} className="">
         <div className="mb-4">
           <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
           <input
@@ -56,6 +66,7 @@ const Profile = () => {
       </form>
       </div>
     </div>
+        </div>
     );
 };
 
